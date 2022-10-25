f758e30d501862e0c4c902ea5c9f482c7ca6bc3c
m-ghiringhelli <mghiring@gmail.com>
2022-10-25 11:27:56 -0700

    reformat fetch-utils to put example code in comments so CI passes

--- fetch-utils.js
+++ fetch-utils.js
@@ -7,25 +7,29 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 export async function createTodo(todo) {
     // create a single incomplete todo with the correct 'todo' property for this user in supabase
 
-    return checkError(response);
+    // once you have a response from supabase, comment this back in:
+    // return checkError(response);
 }
 
 export async function deleteAllTodos() {
     // delete all todos for this user in supabase
 
-    return checkError(response);
+    // once you have a response from supabase, comment this back in:
+    // return checkError(response);
 }
 
 export async function getTodos() {
     // get all todos for this user from supabase
 
-    return checkError(response);
+    // once you have a response from supabase, comment this back in:
+    // return checkError(response);
 }
 
 export async function completeTodo(id) {
     // find the and update (set complete to true), the todo that matches the correct id
 
-    return checkError(response);
+    // once you have a response from supabase, comment this back in:
+    // return checkError(response);
 }
 
 export function getUser() {
